9728a398cd18d81de41940e72170a4a0aab8ad78
Flavio Adrián Andrada Trainini <andrada.flavio@outlook.com>
2019-12-10 12:42:20 -0300

    Variables de entorno para linux

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 /*PUERTO*/
-process.env.PORT = process.env.PORT || 4000;
+process.env.PORT = process.env.PORT || 80;
 
 /* ENTORNO*/
 process.env.NODE_ENV = process.env.NODE_ENV || 'prod';
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 require('./config/config');
 
 const express = require('express');
